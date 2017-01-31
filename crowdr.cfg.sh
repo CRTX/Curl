14a88219d63e7d67c91c42726f039493a408a592
CRTX <ruiz.d.christian@gmail.com>
2017-01-31 17:34:23 -0600

    Changed the container name to better reflect description when running scripts

--- crowdr.cfg.sh
+++ crowdr.cfg.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-crowdr_project="curlobject"
+crowdr_project="curl"
 crowdr_name_format="%s-%s"
 
 crowdr_config="
 #PHP CLI
-cli build docker/php
-cli volume $(pwd):/app
-cli workdir /app
+curlwrapper build docker/php
+curlwrapper volume $(pwd):/app
+curlwrapper workdir /app
 
 "
